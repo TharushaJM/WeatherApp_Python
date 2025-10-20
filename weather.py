e866d13f53e2bef8c1222ad0bee478150e0df205
TharushaGonawala <157585076+TharushaJM@users.noreply.github.com>
2025-10-20 20:43:46 +0530

    v.1.1

---
+++ weather.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from geopy.geocoders import Nominatim        # pip install geopy
+from tkinter import ttk, messagebox
+from timezonefinder import TimezoneFinder    # pip install timezonefinder
+from datetime import *
+import requests                               # pip install requests
+import pytz                                   # pip install pytz
+from PIL import Image, ImageTk                # pip install pillow
+import time
+
+
+class weatheClass
